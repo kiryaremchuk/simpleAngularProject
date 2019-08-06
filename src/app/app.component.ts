29773f3662df15ce8af74516f43fa8c1ebf7a082
heilikpd <heilikpd@gmail.com>
2019-08-06 10:34:13 +0300

    move logic from component to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,25 @@
 import { Component } from "@angular/core";
-import questions from "./questions.json";
+import { DataopeatorService } from "./dataopeator.service";
+//import questions from "./questions.json";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [DataopeatorService]
 })
 export class AppComponent {
-  questions = questions;
-  arrlength: number = questions.length;
-  title: string = "myFirstAngularApp";
-  images = [];
-  texts = [];
+  constructor(private dataopeatorService: DataopeatorService) {}
+  arrlength = this.dataopeatorService.getQuestionsLength();
   errr = false;
-  gen(num: number) {
-    this.images = [];
-    this.texts = [];
-    let newarr = [];
-    let regURL = new RegExp(/^https?:\/\//i);
-    this.errr = false;
-
-    if (num > this.questions.length || num < 0) {
-      this.errr = true;
-      throw new Error("error");
-    }
-    if (num == questions.length) {
-      newarr = this.questions;
-    }
-    while (newarr.length < num) {
-      newarr = this.questions.reduce((acc, task) => {
-        const shouldPush: boolean = !!Math.round(Math.random());
-        return shouldPush && acc.length < num ? [...acc, task] : acc;
-      }, []);
-    }
-    newarr.forEach(e => {
-      if (regURL.test(e)) {
-        this.images.push(e);
-      } else this.texts.push(e);
-    });
+  images: string[];
+  texts: string[];
+  title: string = "myFirstAngularApp";
+  gen(num: number): void {
+    let err = this.dataopeatorService.getError(num);
+    this.errr = err;
+    let newarr = this.dataopeatorService.generateRandomArray(num);
+    let result = this.dataopeatorService.divideArray(newarr);
+    this.images = result.images;
+    this.texts = result.texts;
   }
 }
--- src/app/dataopeator.service.ts
+++ src/app/dataopeator.service.ts
@@ -5,5 +5,45 @@ import questions from "./questions.json";
 })
 export class DataopeatorService {
   constructor() {}
-  private questions: string[] = questions;
+  questions: string[] = questions;
+  // errr = false;
+  getQuestionsLength() {
+    const arrlength: number = questions.length;
+    return arrlength;
+  }
+  getError(num: number) {
+    let errr: boolean;
+    const arrlength: number = questions.length;
+    if (num > arrlength || num < 0) {
+      return (errr = true);
+    }
+  }
+  generateRandomArray(num: number): string[] {
+    let newarr = [];
+    if (num == questions.length) {
+      newarr = this.questions;
+    }
+    if (num <= questions.length) {
+      while (newarr.length < num) {
+        newarr = this.questions.reduce((acc, task) => {
+          const shouldPush: boolean = !!Math.round(Math.random());
+          return shouldPush && acc.length < num ? [...acc, task] : acc;
+        }, []);
+      }
+      return newarr;
+    } else throw new Error("error");
+  }
+  divideArray(arr: string[]) {
+    let arrays = {
+      images: [],
+      texts: []
+    };
+    let regURL = new RegExp(/^https?:\/\//i);
+    arr.forEach(e => {
+      if (regURL.test(e)) {
+        arrays.images.push(e);
+      } else arrays.texts.push(e);
+    });
+    return arrays;
+  }
 }
